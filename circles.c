71911215f72e57a6ffca9d731c038d6ca577ab9d
Zholud <zholud.zher@mail.ru>
2025-09-30 22:28:50 +0300

    relese version

--- circles.c
+++ circles.c
@@ -9,46 +9,43 @@ struct  circle
 void main() {
 	struct circle circle1;
 	struct circle circle2;
-	printf("First circle:\n");
-	printf(" - Enter x coordinate: ");
-	scanf_s("%f", &circle1.x);
-	printf(" - Enter y coordinate: ");
-	scanf_s("%f", &circle1.y);
-	printf(" - Enter radius: ");
-	scanf_s("%f", &circle1.r);
-	printf("------------------------------------\n");
-	printf("Second circle:\n");
-	printf(" - Enter x coordinate: ");
-	scanf_s("%f", &circle2.x);
-	printf(" - Enter y coordinate: ");
-	scanf_s("%f", &circle2.y);
-	printf(" - Enter radius: ");
-	scanf_s("%f", &circle2.r);
+		printf("First circle:\n");
+		printf(" - Enter x coordinate: ");
+		scanf_s("%f", &circle1.x);
+		printf(" - Enter y coordinate: ");
+		scanf_s("%f", &circle1.y);
+		printf(" - Enter radius: ");
+		scanf_s("%f", &circle1.r);
+		printf("------------------------------------\n");
+		printf("Second circle:\n");
+		printf(" - Enter x coordinate: ");
+		scanf_s("%f", &circle2.x);
+		printf(" - Enter y coordinate: ");
+		scanf_s("%f", &circle2.y);
+		printf(" - Enter radius: ");
+		scanf_s("%f", &circle2.r);
 
-	float distance = sqrt((circle1.x - circle2.x) * (circle1.x - circle2.x) + (circle1.y - circle2.y) * (circle1.y - circle2.y));
+		float distance = sqrt((circle1.x - circle2.x) * (circle1.x - circle2.x) + (circle1.y - circle2.y) * (circle1.y - circle2.y));
 
-	if (circle1.r == circle2.r && distance == 0) {
-		printf("circle1 = circle2");
-	}
-	if (distance > circle1.r + circle2.r) {
-		printf("circles no intersect");
-	}
-	if (distance == circle1.r + circle2.r)
-	{
-		printf("circles touches outside");
-	}
-	if (distance < circle1.r + circle2.r)
-	{
-		if (distance > circle1.r && distance > circle2.r) {
-			printf("circles intersects outside");
+		if (circle1.r == circle2.r && distance == 0) {
+			printf("circle1 = circle2");
+		} 
+		else if (distance > circle1.r + circle2.r) {
+			printf("circles no intersect");
+		} 
+		else if (distance == circle1.r + circle2.r) {
+			printf("circles touches outside");
 		}
-		if (distance + circle1.r < circle2.r || distance + circle2.r < circle1.r) {
+		else if (distance > circle1.r && distance > circle2.r) {
+			printf("circles intersects outside");
+		} 
+		else if (distance + circle1.r < circle2.r || distance + circle2.r < circle1.r) {
 			printf("circle in circle");
-		} else if (distance + circle1.r == circle2.r || distance + circle2.r == circle1.r) {
+		}
+		else if (distance + circle1.r == circle2.r || distance + circle2.r == circle1.r) {
 			printf("circles touches inside");
 		}
-		else if (distance <= circle1.r || distance <= circle2.r) { //think about it
+		else { //think about it (distance <= circle1.r || distance <= circle2.r
 			printf("circles intersects inside");
 		}
-	}
 }
