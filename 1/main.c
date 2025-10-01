104122841eda9975975af8436629f0b91425c20e
unknown <T2336@D113.local>
2025-10-01 16:18:48 +0300

    minor changes

---
+++ 1/main.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define BIN_SIZE sizeof(int);
+
+void main() {
+	unsigned int num = 0;
+	scanf("%u", &num);
+	char bit[BIN_SIZE];
+	//for (int i = 0; )
+}
